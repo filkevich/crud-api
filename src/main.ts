f58305514941d2afd944ace751d4b054ed79252a
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-15 19:19:35 +0300

    feat: implement test server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 import 'dotenv/config'
+import http from 'http'
 
-console.log(process.env.PORT)
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<h1>Hello</h1>')
+  res.end()
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log('server is running on port ' + PORT))
